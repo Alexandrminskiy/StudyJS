a0258d6835ddb1bf1144a55763b6c6cf98d8fc6e
alexandrminskiy <alexandrminskiy@gmail.com>
2022-12-19 21:58:08 +0300

    111

--- DAY2/api.js
+++ DAY2/api.js
@@ -1,6 +1,6 @@
 class Api {
     constructor(name) {
-        this.url = 'https://cats.petiteweb.dev/api/single';
+        this.url = 'https://cats.petiteweb.dev/api/single/';
         this.name = name;
     }
 
@@ -8,20 +8,44 @@ class Api {
     getCats() {
         return fetch(`${this.url}${this.name}/show`)
     }
-    getCat()
-    getCatsIds()
+    getCat(id) {
+        return fetch(`${this.url}${this.name}/show/${id}`)
+    }
+    getCatsIds() {
+        return fetch(`${this.url}${this.name}/show`)
+    }
+
+    addCat(bodyOfCat){
+        fetch(`${this.url}${this.name}/add`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'app;ication/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(bodyOfCat)
+    });
+    }
+
+    updCat(changeiPartsOfCat, id){
+        return fetch(`${this.url}${this.name}/update/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Accept': 'app;ication/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(changeiPartsOfCat)
+    });
+    }
+    delCat(id){
+        return fetch(`${this.url}${this.name}/update/${id}`, {
+            method: 'DELETE'
+        });
+    }
 
 
-    addCat()
-    updCat()
-    delCat()
 
-
-
-
+   
 
 
 }
 
-// https://sberbank-school.ru/programs/18135/item/947813
-// 1.50
--- DAY2/index.js
+++ DAY2/index.js
@@ -1,1 +1,82 @@
-api.getcats('alexandrminskiy')
+// api.getcats('alexandrminskiy')
+const getCats = async () => {
+    const responce = await fetch('https://cats.petiteweb.dev/api/single/alexandrminskiy/show', {
+        method: 'GET',
+    });
+    const data = await responce.json();
+
+    console.log(data)
+}
+// getCats()
+
+const newCat = {
+    id: 111,
+    name: "барон",
+    image: "https://telegrator.ru/wp-content/uploads/2021/05/chat_avatar-136.jpg",
+    age: 2,
+    rate: 8,
+    favorite: false,
+    description: "Милый кот"
+}
+
+const addCat = async () => {
+    const responce = await fetch('https://cats.petiteweb.dev/api/single/alexandrminskiy/add', {
+        method: 'POST',
+        headers: {
+            'Accept': 'app;ication/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newCat)
+    });
+    const data = await responce.json();
+    console.log(data)
+}
+
+// addCat()
+
+const changeidObj = { id: 7 }
+// PUT
+const changeCat = async () => {
+    const responce = await fetch('https://cats.petiteweb.dev/api/single/alexandrminskiy/update/11', {
+        method: 'PUT',
+        headers: {
+            'Accept': 'app;ication/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(changeidObj)
+    });
+    const data = await responce.json();
+    console.log(data)
+}
+
+const api = new Api('alexandrminskiy');
+// console.log(api.getCats());
+
+// Промисы
+// api.getCats()
+//     .then(res => res.json())
+//     .then(data => console.log(data))
+
+// Асинки
+const getting = async () => {
+    const res = await api.getCats();
+    const data = await res.json()
+
+    console.log(data);
+}
+getting()
+
+const adding = async (body) => {
+    const res = await api.addCat(body);
+    const data = res.json();
+
+    console.log(data);
+}
+// adding(newCat)
+configured
+const deleting = async (id) => {
+    const res = await api.delCat(id);
+    const data = res.json();
+    // console.log(data);
+}
+deleting(111)
