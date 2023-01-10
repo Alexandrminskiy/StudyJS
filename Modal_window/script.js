4f1710f40a3d54d3279193f2c6ad970af990583f
Alexandrminskiy <alexandrminskiy@gmail.com>
2023-01-10 16:59:06 +0300

    123

--- Modal_window/script.js
+++ Modal_window/script.js
@@ -4,9 +4,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Модальное окно, которое необходимо открыть
     let modal1 = document.querySelector('#modal-1');
-    
+
     // Кнопка "закрыть" внутри модального окна
-    let closeButton = modal1.querySelector('modal__close-button');
+    let closeButton = modal1.querySelector('.modal__close-button');
 
     callBackButton.onclick = function () {
         modal1.classList.add('modal_active');
@@ -15,5 +15,17 @@ document.addEventListener('DOMContentLoaded', () => {
     closeButton.onclick = function () {
         modal1.classList.remove('modal_active');
     }
-    console.log(closeButton)
-});
+
+    // modal1.onmousedown = function (event) {
+    //     let target = event.target;
+    //     let modalContent = modal1.querySelector('.modal__content');
+    //     if (event.target.closest('.' + modalContent.className) === null) {
+    //         this.classList.remove('modal_active'); 
+    // }
+
+// Закрытие окна по клику вне модального окна
+    window.onclick = function (event) {
+        if (event.target == modal1) {
+            modal1.classList.remove('modal_active');
+        }
+    }});
